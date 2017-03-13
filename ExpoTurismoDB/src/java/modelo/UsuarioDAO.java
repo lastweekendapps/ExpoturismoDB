635581f401528a2feec3b1575325ddf18a50ad06
nicolasOtalora <nicolas.otalora@correo.usa.edu.co>
2017-03-12 19:27:52 -0500

    Update UsuarioDAO.java

--- ExpoTurismoDB/src/java/modelo/UsuarioDAO.java
+++ ExpoTurismoDB/src/java/modelo/UsuarioDAO.java
@@ -187,6 +187,7 @@ public class UsuarioDAO {
 
                     rafTree.skipBytes(20);
                     rafTree.writeLong(pos);
+                    flag = true;
 
                 }
             }
